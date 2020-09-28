ba364b760b75fe3e8ef868e7f3daaf984080655d
Marques Avery <marquesavery@Marquess-MacBook-Pro-2.local>
2020-09-28 17:23:53 -0500

    Documentation

--- lib/nfl_schedule/cli.rb
+++ lib/nfl_schedule/cli.rb
@@ -4,11 +4,11 @@ class NflSchedule::CLI
     menu
   end
 
-  def schedule(url = "https://www.espn.com/nfl/schedule")
+  def schedule(url = "https://www.espn.com/nfl/schedule") #list the NFL Schedule
 
     puts "This week's Schedule."
-    @schedule = NflSchedule::Schedule.scrape_espn(url)
-    @schedule.each.with_index(1) do |game, i|
+    @schedule = NflSchedule::Schedule.scrape_espn(url) #calls the scrape method from the schedule class
+    @schedule.each.with_index(1) do |game, i| #iterates through the games array and lists the games
       if game.day != nil
         puts "#{game.day}"
       end
@@ -16,33 +16,33 @@ class NflSchedule::CLI
     end
   end
 
-  def menu
+  def menu #lists options for user
     input = nil
-    while input != "exit"
+    while input != "exit" #continues until the user is done
       puts "Enter the number of the game you'd like more info, type list for the full schedule, or type exit to leave:"
       input = gets.strip.downcase
 
-      if input.to_i > 0 && input.to_i <= 16
-        the_game = @schedule[input.to_i-1]
+      if input.to_i > 0 && input.to_i <= 16 #Max of 16 games a week (32 teams)
+        the_game = @schedule[input.to_i-1] #calls and assigns the game to a variable
         puts "#{the_game.team1} at #{the_game.team2}, #{the_game.location}."
-        odds = NflSchedule::Schedule.scrape_game(the_game.url)
+        odds = NflSchedule::Schedule.scrape_game(the_game.url) #scrapes the specific game called and lists the odds
         puts "Spread"
         puts "#{odds[0]}      #{odds[1]}"
         puts "Money Line"
         puts "#{odds[2]}    #{odds[3]}"
         puts "Over/Under"
         puts "#{odds[4]}"
-      elsif input == "list"
+      elsif input == "list" #if user input is list it will show the schedule again
         schedule
-      elsif input == "exit"
+      elsif input == "exit" #will cause the application to stop
         goodbye
       else
-        puts "Not sure what you want, type list or exit:"
+        puts "Not sure what you want, type list or exit:" #lets user know the input is invalid
       end
     end
   end
 
-  def goodbye
+  def goodbye #shows user the application is closing
     puts "Enjoy watching the NFL!"
   end
 
